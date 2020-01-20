594ad86f151b4aa44e8e8e0e7459162f24a8cf18
glqiao971102 <44012470+glqiao971102@users.noreply.github.com>
2020-01-21 06:52:50 +0800

    add appendPageLinks function, but fail in the addEvent in link a

--- js/script.js
+++ js/script.js
@@ -53,7 +53,7 @@ function showPage(list, page) {
 
 }
 
-showPage(list_item, 2)
+
 
 
 /*** 
@@ -61,8 +61,61 @@ showPage(list_item, 2)
    functionality to the pagination buttons.
 ***/
 
+const appendPageLinks = (list_item) => {
+   /*
+   1. Determine how many pages are needed for the list by dividing the
+   total number of list items by the max number of items per page
+   2. Create a div, give it the “pagination” class, and append it to the .page div
+   3. Add a ul to the “pagination” div to store the pagination links
+
+   <ul>
+      <li>
+         <a class="active" href="#">1</a>
+      </li>
+
+   4. for every page, add li and a tags with the page number text
+   5. Add an event listener to each a tag. When they are clicked
+   call the showPage function to display the appropriate page
+   6. Loop over pagination links to remove active class from all links
+   7. Add the active class to the link that was just clicked. You can identify that
+   clicked link using event.target
+   */
+   }
+   const numOfPage = list_item.length / number_of_item
+   const pageDiv = document.getElementsByClassName('page')
+   const div = document.createElement('div')
+   div.className = 'pagination';
+   pageDiv.appendChild(div)
+   const ul = document.createElement('ul')
+   for (let i = 0; i < numOfPage; i ++){
+
+      const li = document.createElement('li');
+      ul.appendChild(li);
+      const a = document.createElement('a');
+      a.className = 'active'
+      a.href = '#';
+      a.textContent = i + 1
+      li.appendChild(a)
+
+      a.addEventListener('click', e => {
+
+         const allA = document.querySelectorAll('a')
+
+         for(let i = 0; i < allA.length; i++ ){
+
+            
+
+         }
 
 
 
+
+
+      })
+   }
+
+
+
+showPage(list_item, 2)
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
